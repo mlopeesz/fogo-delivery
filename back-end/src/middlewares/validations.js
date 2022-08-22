0d08ae3e6bd923bdc4a2d06367d5a71b833eb62f
Mauricio <mlopeesz@gmail.com>
2022-08-22 18:59:35 -0300

    feat: :sparkles: add and import validations to userRoutes

--- back-end/src/middlewares/validations.js
+++ back-end/src/middlewares/validations.js
@@ -1,10 +1,23 @@
 const userService = require('../services/userService');
+const md5 = require('md5');
 
-const checkIfUserExists = async (req, res) => {
+const checkIfUserExists = async (req, res, next) => {
   const { email } = req.body;
   const user = await userService.findByEmail(email);
-  if (user) return res.status(200).json(user);
-  return res.status(404).json({ message: 'Usuário não encontrado.' });
+  if (!user) return res.status(404).json({ message: 'Usuário não encontrado.' });
+  next();
 };
 
-module.exports = checkIfUserExists;
+const checkUserPassword = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await userService.findByEmail(email);
+  const encryptedPassword = md5(password);
+  
+  if (user.password !== encryptedPassword) {
+    return res.status(403).json({ message: 'Senha incorreta.' });
+  };
+
+  next();
+};
+
+module.exports = { checkIfUserExists, checkUserPassword };
