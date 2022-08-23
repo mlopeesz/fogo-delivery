8245bef489668771a01412103b1de4f43dcda526
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-23 16:53:13 -0300

    chore: :sparkles: new constant STATUS_OK

--- front-end/src/constants/index.js
+++ front-end/src/constants/index.js
@@ -1,4 +1,5 @@
 const MIN_LENGTH_PASSWORD = 6;
 const validateEmailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+const STATUS_OK = 200;
 
-export { MIN_LENGTH_PASSWORD, validateEmailRegex };
+export { MIN_LENGTH_PASSWORD, validateEmailRegex, STATUS_OK };
