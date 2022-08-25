8b9f49e06667827531ed1ae26f7ac66ed844ca88
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-25 12:14:29 -0300

    feat: :sparkles: check if user is already registered

--- back-end/src/middlewares/validations.js
+++ back-end/src/middlewares/validations.js
@@ -1,5 +1,5 @@
-const userService = require('../services/userService');
 const md5 = require('md5');
+const userService = require('../services/userService');
 
 const checkIfUserExists = async (req, res, next) => {
   const { email } = req.body;
@@ -15,9 +15,16 @@ const checkUserPassword = async (req, res, next) => {
   
   if (user.password !== encryptedPassword) {
     return res.status(403).json({ message: 'Senha incorreta.' });
-  };
+  }
 
   next();
 };
 
-module.exports = { checkIfUserExists, checkUserPassword };
+const checkIfUserExistsRegister = async (req, res, next) => {
+  const { email } = req.body;
+  const user = await userService.findByEmail(email);
+  if (user) return res.status(409).json({ message: 'Usuário já existe.' });
+  next();
+};
+
+module.exports = { checkIfUserExists, checkUserPassword, checkIfUserExistsRegister };
