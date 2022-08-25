6d1481eba380aa30630d17aa4ee2c99b489e9605
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-25 13:58:34 -0300

    feat: :sparkles:
    /customer/products route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
+      <Route exact path="/customer/products" />
     </Routes>
   );
 }
