d469ded9c020b5a08cfcfa7af80891dce7d317ee
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-25 12:14:48 -0300

    feat: :sparkles: register route

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const Router = require('express');
-const { checkIfUserExists, checkUserPassword } = require('../middlewares/validations');
-const login = require('../controllers/userController');
+const {
+  checkIfUserExists,
+  checkUserPassword,
+  checkIfUserExistsRegister,
+} = require('../middlewares/validations');
+const { login, create } = require('../controllers/userController');
 
 const route = Router();
 
 route.post('/login', checkIfUserExists, checkUserPassword, login);
+route.post('/register', checkIfUserExistsRegister, create);
 
 module.exports = route;
