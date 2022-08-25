0ba6a964cfc80203f772962317efec6cafb5fe01
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-25 10:03:13 -0300

    feat: :sparkles: Register page and import in App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
     </Routes>
   );
 }
