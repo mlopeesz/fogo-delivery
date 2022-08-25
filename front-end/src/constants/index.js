79ceb615afea8308ecd0d89e5bfe25aa987880f4
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-25 13:59:00 -0300

    feat: :sparkles: STATUS_CREATED constant

--- front-end/src/constants/index.js
+++ front-end/src/constants/index.js
@@ -2,3 +2,4 @@ export const MIN_LENGTH_PASSWORD = 6;
 export const MIN_LENGTH_NAME = 12;
 export const validateEmailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
 export const STATUS_OK = 200;
+export const STATUS_CREATED = 201;
