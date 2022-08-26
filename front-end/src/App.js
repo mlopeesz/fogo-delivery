1cd59093ca68d19d180a4a430bc1cd637f7a6fd2
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-26 10:24:15 -0300

    feat: :sparkles: CustomerProducts initial structure and import in App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import CustomerProducts from './pages/CustomerProducts';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
@@ -10,7 +11,7 @@ function App() {
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
-      <Route exact path="/customer/products" />
+      <Route exact path="/customer/products" element={ <CustomerProducts /> } />
     </Routes>
   );
 }
