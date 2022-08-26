8044734ab476fdca5514094d99c89380e17ac229
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-26 11:42:22 -0300

    feat: :sparkles: add getAllProducts function and render product cards

--- front-end/src/pages/CustomerProducts/index.jsx
+++ front-end/src/pages/CustomerProducts/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
+import ProductCard from '../../components/ProductCard';
+import { api } from '../../services/api';
 
 function CustomerProducts() {
+  const [products, setProducts] = useState();
+
+  const getAllProducts = async () => {
+    const response = await api.get('/customer/products');
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  });
+
   return (
     <div>
       <Navbar />
+      {products?.map((product, index) => (
+        <ProductCard
+          key={ index }
+          id={ product.id }
+          name={ product.name }
+          price={ product.price }
+          image={ product.urlImage }
+        />
+      ))}
     </div>
   );
 }
