f3b4306bd3f7f5f9a7ed30a38a17596d4c68c36a
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-26 10:20:38 -0300

    feat: :sparkles: customer routes and import in app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('../routes/userRoutes');
+const customerRoutes = require('../routes/customerRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
+app.use(customerRoutes);
 
 module.exports = app;
