4a63cc6a1c52112eac68fa3489ccf55c41cfba2c
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-29 10:17:21 -0300

    feat: :sparkles: handleLogout function and user name in Navbar

--- front-end/src/components/Navbar/index.jsx
+++ front-end/src/components/Navbar/index.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { removeLocalStorage } from '../../utils/localStorage';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    removeLocalStorage('user');
+    navigate('/');
+  };
+
+  const getUserName = () => {
+    const userLocalStorage = JSON.parse(localStorage.getItem('user'));
+    return userLocalStorage.name;
+  };
+
   return (
     <nav>
       <div>
@@ -8,8 +22,20 @@ function Navbar() {
         <p data-testid="customer_products__element-navbar-link-orders">MEUS PEDIDOS</p>
       </div>
       <div>
-        <p data-testid="customer_products__element-navbar-user-full-name">USUÁRIO</p>
-        <p data-testid="customer_products__element-navbar-link-logout">SAIR</p>
+        <p
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          { getUserName() }
+
+        </p>
+        <button
+          type="button"
+          data-testid="customer_products__element-navbar-link-logout"
+          onClick={ handleLogout }
+        >
+          SAIR
+
+        </button>
       </div>
     </nav>
   );
