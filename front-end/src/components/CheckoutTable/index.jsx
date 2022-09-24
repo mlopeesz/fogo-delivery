23ba888a46b40cde14e4c07d854fe935e9dcbf7f
Mauricio Lopes <mlopeesz@gmail.com>
2022-09-24 12:26:37 -0300

    feat: :construction: customer checkout page initial structure

---
+++ front-end/src/components/CheckoutTable/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function CheckoutTable() {
+  return (
+    <table>
+      <tr>
+        <th>Item</th>
+        <th>Descrição</th>
+        <th>Quantidade</th>
+        <th>Valor Unitário</th>
+        <th>Sub-total</th>
+        <th>Remover Item</th>
+      </tr>
+    </table>
+  );
+}
+
+export default CheckoutTable;
---
+++ front-end/src/pages/CustomerCheckout/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CheckoutTable from '../../components/CheckoutTable';
+import Navbar from '../../components/Navbar';
+
+function CustomerCheckout() {
+  return (
+    <div>
+      <Navbar />
+      <CheckoutTable />
+    </div>
+  );
+}
+
+export default CustomerCheckout;
