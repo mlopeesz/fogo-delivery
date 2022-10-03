5177f57d92168c5ba9b7e41dd2523b0dfeb16b37
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-03 09:02:31 -0300

    refactor: :sparkles: remove CheckoutTable component

--- front-end/src/pages/CustomerCheckout/index.jsx
+++ front-end/src/pages/CustomerCheckout/index.jsx
@@ -1,19 +1,97 @@
-import React, { useEffect } from 'react';
-import CheckoutTable from '../../components/CheckoutTable';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
 import { formatToBRL } from '../../constants';
 
 function CustomerCheckout() {
-  const totalPrice = localStorage.getItem('totalPriceCart');
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [cart, setCart] = useState([]);
+
+  const removeItem = (item) => {
+    const cartUpdated = cart.filter((element) => element.id !== item.id);
+    let total = 0;
+    cartUpdated.forEach(({ quantity, price }) => {
+      total += quantity * price;
+    });
+    localStorage.setItem('cart', JSON.stringify(cartUpdated));
+    setCart(cartUpdated);
+    setTotalPrice(total);
+  };
 
   useEffect(() => {
-  }, [totalPrice]);
+    setCart(JSON.parse(localStorage.getItem('cart')));
+    const priceTot = cart.reduce((sum, elm) => {
+      const value = elm.quantity * elm.price;
+      return sum + value;
+    }, 0);
+    setTotalPrice(priceTot);
+  }, [cart]);
 
   return (
     <div>
       <div>
         <Navbar />
-        <CheckoutTable />
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            <th>Remover Item</th>
+          </tr>
+          {cart.map((item, index) => (
+            <tr key={ index }>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-item-number-${index}`
+                }
+              >
+                {index + 1}
+
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-name-${index}` }
+              >
+                {item.name}
+
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+              >
+                {item.quantity}
+
+              </td>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-unit-price-${index}`
+                }
+              >
+                {formatToBRL.format(item.price)}
+
+              </td>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-sub-total-${index}`
+                }
+              >
+                {formatToBRL.format(item.price * item.quantity)}
+
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+              >
+                <button
+                  type="button"
+                  onClick={ () => removeItem(item) }
+                >
+                  Remover item
+
+                </button>
+
+              </td>
+            </tr>
+          ))}
+        </table>
         <span
           data-testid="customer_checkout__element-order-total-price"
         >
--- front-end/src/pages/CustomerProducts/index.jsx
+++ front-end/src/pages/CustomerProducts/index.jsx
@@ -4,7 +4,6 @@ import Navbar from '../../components/Navbar';
 import ProductCard from '../../components/ProductCard';
 import { formatToBRL } from '../../constants';
 import { api } from '../../services/api';
-import { setLocalStorage } from '../../utils/localStorage';
 import './style.scss';
 
 function CustomerProducts() {
@@ -26,7 +25,6 @@ function CustomerProducts() {
     });
     setDisableButton(!total);
     setTotalPrice(total);
-    setLocalStorage('totalPriceCart', total);
   };
 
   useEffect(() => {
