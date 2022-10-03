ad9cb9c87f10b3a0387de189125bbfbe8ac08e8f
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-03 18:09:43 -0300

    feat: :sparkles: create and createSalesProduct function

---
+++ back-end/src/services/saleService.js
@@ -0,0 +1,14 @@
+const { sale, salesProduct } = require('../database/models');
+
+
+const create = async (data) => {
+  const response = await sale.create(data);
+  return response;
+};
+
+const createSalesProduct = async (data) => {
+  const createNewSaleProducts = await salesProduct.create(data);
+  return createNewSaleProducts;
+};
+
+module.exports = { create, createSalesProduct };
