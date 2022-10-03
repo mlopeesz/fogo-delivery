58d58207d051fc9a22357e2a42ce6069a62712fb
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-03 18:10:28 -0300

    feat: :sparkles: import saleRoutes in app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('../routes/userRoutes');
 const customerRoutes = require('../routes/customerRoutes');
+const saleRoutes = require('../routes/saleRoutes')
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use(express.static('src'));
 app.use(userRoutes);
 app.use(customerRoutes);
+app.use(saleRoutes);
 
 module.exports = app;
