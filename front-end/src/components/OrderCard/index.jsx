a37c97fd56b94ad02d772568ee7a5c099da5e1ed
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-06 10:13:53 -0300

    feat: :sparkles: finish CustomerOrders page and OrderCard component

--- front-end/src/components/OrderCard/index.jsx
+++ front-end/src/components/OrderCard/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { formatDate } from '../../utils/utils';
+import { formatToBRL } from '../../constants';
 
-function OrderCard() {
+function OrderCard({ orderId, orderStatus, orderDate, orderPrice }) {
   return (
     <div>
-      <p data-testid="customer_orders__element-order-id-<id>">ORDER XXXX</p>
-      <p data-testid="customer_orders__element-delivery-status-<id>">STATUS</p>
-      <p data-testid="customer_orders__element-order-date-<id>">DATA</p>
-      <p data-testid="customer_orders__element-card-price-<id>">PRICE</p>
+      <p data-testid={ `customer_orders__element-order-id-${orderId}` }>{orderId}</p>
+      <p
+        data-testid={ `customer_orders__element-delivery-status-${orderId}` }
+      >
+        {orderStatus}
+
+      </p>
+      <p
+        data-testid={ `customer_orders__element-order-date-${orderId}` }
+      >
+        {formatDate(orderDate)}
+
+      </p>
+      <p
+        data-testid={ `customer_orders__element-card-price-${orderId}` }
+      >
+        {formatToBRL.format(orderPrice)}
+
+      </p>
     </div>
   );
 }
+
+OrderCard.propTypes = {
+  orderId: PropTypes.number.isRequired,
+  orderStatus: PropTypes.string.isRequired,
+  orderDate: PropTypes.string.isRequired,
+  orderPrice: PropTypes.string.isRequired,
+};
 
 export default OrderCard;
