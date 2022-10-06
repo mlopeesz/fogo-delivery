ab9a177b5b094d3b7fbb2e703b51c35ae7807e82
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-06 10:12:39 -0300

    feat: :recycle: return response with user id

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -4,8 +4,8 @@ const { generateToken } = require('../auth/jwt');
 const login = async (req, res) => {
   const user = await userService.findByEmail(req.body.email);
   const token = generateToken(user);
-  const { name, email, role } = user;
-  const response = { name, email, role, token };
+  const { id, name, email, role } = user;
+  const response = { id, name, email, role, token };
   return res.status(200).json(response);
 };
 
